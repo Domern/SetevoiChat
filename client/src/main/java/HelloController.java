c58d5f5c0ec32f2cfaa7875f5f3304b02818c078
Ярослав <zzzqqqzzzwww@yandex.ru>
2022-09-18 23:58:12 +0300

    1

--- client/src/main/java/HelloController.java
+++ client/src/main/java/HelloController.java
@@ -1,8 +1,6 @@
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.net.URL;
@@ -11,6 +9,12 @@ import java.util.ResourceBundle;
 public class HelloController implements Initializable {
     private Connection connection;
 
+
+    public TextField loginField;
+    public PasswordField passField;
+    public TextField serviceField;
+    public Button sendAuthButton;
+    public Button sendMsgButtom;
     public TextField textField;
     public TextArea textArea;
     public ListView listView;
@@ -29,11 +33,30 @@ public class HelloController implements Initializable {
         try {
             while (true) {
                 String msg = connection.inputStream().readUTF();
-                textArea.setText(msg+"\n"+textArea.getText());
+                if(msg.startsWith("#authok")){
+                    authOk(msg);
+                }else {
+                    serviceField.setText(msg);
+                    textArea.setText(msg + "\n" + textArea.getText());
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void authOk(String msg){
+        loginField.setVisible(false);
+        passField.setVisible(false);
+        serviceField.setVisible(false);
+        sendAuthButton.setVisible(false);
+
+        textArea.setVisible(true);
+        textField.setVisible(true);
+        sendMsgButtom.setVisible(true);
+        listView.setVisible(true);
+
+        String[] split = msg.split("\\s");
     }
 
     public void sendMsg(ActionEvent actionEvent) {
@@ -44,5 +67,15 @@ public class HelloController implements Initializable {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void sendAuth(ActionEvent actionEvent) {
+        String autf="#auth "+loginField.getText()+" "+passField.getText();
+        try {
+            connection.outputStream().writeUTF(autf);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
